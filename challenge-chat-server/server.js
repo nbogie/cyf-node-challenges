0460d64725e979bbba8cd55b125d3489046caffe
mairacagri <mairacagri@gmail.com>
2022-05-19 22:43:57 +0100

    chat server app

--- challenge-chat-server/server.js
+++ challenge-chat-server/server.js
@@ -1,9 +1,9 @@
-const express = require ('express');
+const express = require('express');
 
 const app = express();
 const PORT = 4000;
 
-app.use(express(json));
+// app.use(express(json));
 
 
 const welcomeMessage = {
